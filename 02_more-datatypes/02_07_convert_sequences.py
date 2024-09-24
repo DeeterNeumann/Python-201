1cd1e0f50f71ea90db07cf6a4dfba5414007a47b
Deeter Neumann <drneumann@uwalumni.com>
2024-09-24 10:48:53 -0700

    list-tuple converter

--- 02_more-datatypes/02_07_convert_sequences.py
+++ 02_more-datatypes/02_07_convert_sequences.py
@@ -5,3 +5,19 @@
 # - Convert the list back into a tuple.
 
 string = "codingnomads"
+
+string_tup = tuple(string)
+
+print(string_tup)
+
+string_list = list(string_tup)
+
+print(string_list)
+
+string_list[0] = "k"
+
+print(string_list)
+
+string_tup2 = tuple(string_list)
+
+print(string_tup2)
