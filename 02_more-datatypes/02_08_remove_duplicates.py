32ebe34fe53a15b3d8941e63fff5434c5c366b3f
Deeter Neumann <drneumann@uwalumni.com>
2024-09-24 11:36:31 -0700

    dups gone

--- 02_more-datatypes/02_08_remove_duplicates.py
+++ 02_more-datatypes/02_08_remove_duplicates.py
@@ -4,3 +4,15 @@
 # 2. Use a loop and a second list to solve it more manually
 
 list_ = [1, 2, 3, 4, 3, 4, 5]
+
+dups_gone = set(list_)
+
+print(dups_gone)
+
+dups_gone_again = []
+
+for n in list_:
+    if n not in dups_gone_again:
+        dups_gone_again.append(n)
+
+print(dups_gone_again)
