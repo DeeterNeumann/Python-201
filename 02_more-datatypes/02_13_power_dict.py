161f4fb1884ceae0aef9121a1b2f6eef0dfa5576
Deeter Neumann <drneumann@uwalumni.com>
2024-09-24 13:47:30 -0700

    power dictionary

--- 02_more-datatypes/02_13_power_dict.py
+++ 02_more-datatypes/02_13_power_dict.py
@@ -1,3 +1,13 @@
 # Write a script that creates a dictionary of keys, `n`
 # and values `n * n` for numbers 1 to 10. For example:
 # result = {1: 1, 2: 4, 3: 9, ... and so on}
+
+keys_squared = {}
+
+n = 1
+
+while n < 11:
+    keys_squared[n] = n*n
+    n += 1
+
+print(keys_squared)
