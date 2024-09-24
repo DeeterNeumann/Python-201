8c53572fd838bdabc9faaaa3e676a1a07b88e6b8
Deeter Neumann <drneumann@uwalumni.com>
2024-09-24 10:44:42 -0700

    words tupled

--- 02_more-datatypes/02_06_tupled_words.py
+++ 02_more-datatypes/02_06_tupled_words.py
@@ -4,3 +4,13 @@
 
 # input = "hello world"
 # result_list = [('h', 'e', 'l', 'l', 'o'), ('w', 'o', 'r', 'l', 'd')]
+
+sentence = input("Type a sentence here: ")
+
+splits = []
+
+for word in sentence.split():
+    word_split = tuple(word)
+    splits.append(word_split)
+
+print(splits)
