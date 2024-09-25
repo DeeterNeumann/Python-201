278943e558f35141a2949f71686847e048c6b907
Deeter Neumann <drneumann@uwalumni.com>
2024-09-25 10:57:50 -0700

    largest redone

--- 02_more-datatypes/02_02_product_largest.py
+++ 02_more-datatypes/02_02_product_largest.py
@@ -8,3 +8,29 @@
 from resources import randlist
 
 print(randlist)
+
+#sort random list
+randlist.sort()
+
+#print largest number
+print(randlist[-1])
+
+#for loop for finding largest number (use of none for maximum variable?)
+maximum = randlist[0]
+
+for i in randlist:
+    if i > maximum:
+        maximum = i
+
+print(maximum)
+
+product = 1 #accumulator
+
+for i in randlist:
+    product *= i
+
+print(product)
+    
+
+    
+
