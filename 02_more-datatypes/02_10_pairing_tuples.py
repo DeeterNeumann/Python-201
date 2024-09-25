8103fe6804ff46eca9b2afb3673713a74a6e4d2e
Deeter Neumann <drneumann@uwalumni.com>
2024-09-25 11:00:31 -0700

    tuples paired

--- 02_more-datatypes/02_10_pairing_tuples.py
+++ 02_more-datatypes/02_10_pairing_tuples.py
@@ -17,3 +17,25 @@ from resources import randlist
 print(randlist)
 
 # Write your code below here
+
+randlist.sort() #.sort doesn't return anything
+
+print(randlist)
+
+sorted = randlist
+
+if len(sorted) % 2 != 0:
+    sorted.append(0)
+
+tuples_of_two = []
+
+pair = []
+
+for index in range(0, len(sorted), 2):
+    pair = sorted[index:index+2]
+    # if len(pair) != 2:
+    #     pair.append(0)
+    pair = tuple(pair)
+    tuples_of_two.append(pair)
+
+print(tuples_of_two)
