1e1b97f6690f364db85f15fe047014760e35dd8e
Deeter Neumann <drneumann@uwalumni.com>
2024-09-26 20:22:22 -0700

    characters counted in dictionary

--- 02_more-datatypes/02_14_char_count_dict.py
+++ 02_more-datatypes/02_14_char_count_dict.py
@@ -4,3 +4,30 @@
 #
 # user_input = "hello"
 # result = {"h": 1, "e": 1, "l": 2, "o": 1}
+
+word = input("Please type a word here: ")
+
+splits = list()
+
+letter_count = {}
+
+for char in word:
+    splits.append(char)
+    if char not in letter_count:
+        letter_count[char] = 1
+    else:
+        letter_count[char] += 1
+
+print(letter_count)
+
+
+# letter_count = {}
+
+# iterate over word
+# add individual character as dict key in letter_count
+# count the number of times each character appears in the word and add as dictionary value to respective character
+
+# for char in word:
+#     letter_count[] = char
+
+# print(letter_count)
