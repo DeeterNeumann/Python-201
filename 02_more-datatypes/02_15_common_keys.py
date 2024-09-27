40d09b93847aea5cce674c359b5e30d8d4abb23f
Deeter Neumann <drneumann@uwalumni.com>
2024-09-27 11:04:27 -0700

    Common keys in combined dict

--- 02_more-datatypes/02_15_common_keys.py
+++ 02_more-datatypes/02_15_common_keys.py
@@ -9,3 +9,21 @@
 
 dict_1 = {"a": 1, "b": 2, "c": 3}
 dict_2 = {"a": 2, "c": 4 , "d": 2}
+
+combined_dict = {}
+
+for letter in dict_1:
+    if letter in dict_2:
+        combined_dict[letter] = dict_1[letter] + dict_2[letter]
+    elif letter not in dict_2:
+        combined_dict[letter] = dict_1[letter]
+    else:
+        combined_dict[letter] = dict_2[letter]
+
+for letter in dict_2:
+    if letter in combined_dict:
+        continue
+    if letter not in combined_dict:
+        combined_dict[letter] = dict_2[letter]
+
+print(combined_dict)
