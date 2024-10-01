fd6c01d22923888fdf09095dd00e4a1d9d4efadc
Deeter Neumann <drneumann@uwalumni.com>
2024-10-01 15:11:23 -0700

    unique or not

--- 02_more-datatypes/02_03_unique.py
+++ 02_more-datatypes/02_03_unique.py
@@ -1,11 +1,25 @@
 # Write code that creates a list of all unique values in a list.
 # For example:
 #
-list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
-unique_list = [55, 'hi', 4, 13]
+# list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
+# unique_list = [55, 'hi', 4, 13]
 
-list_3 = list_ + unique_list
+list_of_interest = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
 
-unique = list(set(list_3))
+unique_or_not = {}
 
-print(unique)
+# last_value = ""
+
+for value in list_of_interest:
+    if value in unique_or_not:
+        unique_or_not[value] += 1
+    else:
+        unique_or_not[value] = 1
+
+unique_list = []
+
+for value in list_of_interest:
+    if unique_or_not[value] == 1:
+        unique_list.append(value)
+
+print(unique_list)
