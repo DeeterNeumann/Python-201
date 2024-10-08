f03bfde4c7fd69729612a2a3225804a386c90137
Deeter Neumann <drneumann@uwalumni.com>
2024-10-08 15:19:27 -0700

    more than 20

--- 03_file-input-output/03_01_long_words.py
+++ 03_file-input-output/03_01_long_words.py
@@ -1,2 +1,18 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+
+words = []
+
+with open("words.txt", "r") as fin:
+    for word in fin.readlines():
+        word = word.rstrip()
+        words.append(word)
+
+more_than_20 = []
+
+for w in words:
+    if len(w) > 20:
+        more_than_20.append(w)
+
+with open("words.txt", "w") as fout:
+    fout.write("\n".join(more_than_20))
