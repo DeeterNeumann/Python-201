aa19d3c51d434a12a3dc803f3506eb8729561527
Deeter Neumann <drneumann@uwalumni.com>
2024-10-08 21:22:47 -0700

    words reversed

--- 03_file-input-output/03_03_writing.py
+++ 03_file-input-output/03_03_writing.py
@@ -1,2 +1,19 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+
+
+words = []
+
+with open("all_words.txt", "r") as fin:
+    for word in fin.readlines():
+        word = word.rstrip()
+        words.append(word)
+
+# print(words)
+
+words.reverse() # why can't I assign this to a new variable? e.g., words_reversed = words.reverse()
+
+# print(words)
+
+with open("all_words_reverse.txt", "w+") as fout:
+    fout.write("\n".join(words))
