85b72202ffc356296eeb2449e1fa3d9a8b79e2ec
Deeter Neumann <drneumann@uwalumni.com>
2024-10-17 19:17:31 -0700

    greet in letter

--- 04_functions-and-scopes/04_04_combine_functions.py
+++ 04_functions-and-scopes/04_04_combine_functions.py
@@ -2,3 +2,13 @@
 # with the `write_letter()` function from the previous exercise.
 # Write both functions in this script and call `greet()` within `write_letter()`
 # to let `greet()` take care of creating the greeting string.
+
+
+def write_letter(greeting, name, text):
+    def greet(greeting):
+        sentence = f"{greeting}, {name}! How are you?"
+        return sentence
+    letter = greet(greeting) + f" {text}. Goodbye, {name}."
+    return letter
+
+print(write_letter("Hello", "Deeter", "Did you figure out how to get this code to work?"))
