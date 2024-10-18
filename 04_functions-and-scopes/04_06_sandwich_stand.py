463833870f9fc275fc96eb59348dbf0752d998ca
Deeter Neumann <drneumann@uwalumni.com>
2024-10-17 20:06:17 -0700

    sammy

--- 04_functions-and-scopes/04_06_sandwich_stand.py
+++ 04_functions-and-scopes/04_06_sandwich_stand.py
@@ -3,3 +3,21 @@
 # - takes an arbitrary amount of toppings
 # - returns a string representing a sandwich with the bread on top
 #   and bottom, and the toppings in between.
+
+def make_sandwich(bread, toppings):
+    """Make a sandwich.
+    
+    Args:
+        bread (str): Enter a type of bread e.g., wheat
+        toppings (str): Enter any amount of toppings e.g., ham, cheese, mayo
+    
+    Returns:
+        sammy = the type of bread on the bottom, toppings, and another slice of bread on top.
+    """
+    sammy = f"""
+    {bread}
+    {toppings}
+    {bread}"""
+    return sammy
+
+print(make_sandwich("wheat", "turkey, bacon, cheddar, lettuce, tomatoes, peppers, avacado, mayo"))
