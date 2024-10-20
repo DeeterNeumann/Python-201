df9e81620fdc142769974cb84f317448a52c7b54
Deeter Neumann <drneumann@uwalumni.com>
2024-10-19 22:27:26 -0700

    type hints

--- 04_functions-and-scopes/04_10_type_annotations.py
+++ 04_functions-and-scopes/04_10_type_annotations.py
@@ -1,12 +1,13 @@
 # Add type annotations to the three functions shown below.
+# use the type and the arrow? e.g., def greet(greeting: str, name: str) -> str:
 
-def multiply(num1, num2):
+def multiply(num1: int, num2: int):
     return num1 * num2
 
-def greet(greeting, name):
+def greet(greeting: str, name: str):
     sentence = f"{greeting}, {name}! How are you?"
     return sentence
 
-def shopping_list(*args):
+def shopping_list(*args) -> str:
     [print(f"* {item}") for item in args]
     return args
