1704cba75cfa59420872d56f0cea0c0663acce74
Deeter Neumann <drneumann@uwalumni.com>
2024-10-19 22:38:43 -0700

    homes

--- 04_functions-and-scopes/04_11_your_realtor.py
+++ 04_functions-and-scopes/04_11_your_realtor.py
@@ -3,3 +3,12 @@
 # is why your function should be able to take an arbitrary amount of
 # keyword arguments, and display them all in a list form with some 
 # introductory information.
+
+def house_list(**kwargs):
+    listings = ""
+    for house, amenities in kwargs.items():
+        description = f"{house}: {amenities}"
+        listings += description + "; "
+    return listings
+
+print(house_list(Union_Street = "ground floor, loft, single bathroom, half shower", Cedar_Street = "7th floor, two bedroom, large patio, hot tub"))
