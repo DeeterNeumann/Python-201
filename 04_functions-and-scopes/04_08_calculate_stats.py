bc350b33d106ba6b3b6869818eea858cb37bd153
Deeter Neumann <drneumann@uwalumni.com>
2024-10-19 21:30:03 -0700

    stats

--- 04_functions-and-scopes/04_08_calculate_stats.py
+++ 04_functions-and-scopes/04_08_calculate_stats.py
@@ -4,8 +4,39 @@
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
-  # define the function here
-  pass
+def stats(example_list):
+  def maximum(example_list):
+    max = None
+    for number in example_list:
+      if max is None:
+        max = number
+      elif number > max:
+        max = number
+    maximum = f"The maximum is {max}."
+    return maximum
+  def minimum(example_list):
+    min = None
+    for number in example_list:
+      if min is None:
+        min = number
+      elif number < min:
+        min = number
+    minimum = f"The minimum is {min}."
+    return minimum
+  def average_sum(example_list):
+    count = 0
+    sum = 0
+    for number in example_list:
+      count = count + 1
+      sum = sum + number
+    tot_sum = sum
+    average = sum / count
+    avg_sum = f"The average is {average}. The sum is {tot_sum}."
+    return avg_sum
+  max_stats = maximum(example_list)
+  min_stats = minimum(example_list)
+  avg_sum_stats = average_sum(example_list)
+  statistics = f"{max_stats} {min_stats} {avg_sum_stats}"
+  return statistics
 
-# call the function below here
+print(stats(example_list))
