4385c1d34ddfd8d4560de8b03c53188b1ef54408
Deeter Neumann <drneumann@uwalumni.com>
2024-10-27 10:13:00 -0700

    dinner invite

--- 04_functions-and-scopes/04_09_three_functions.py
+++ 04_functions-and-scopes/04_09_three_functions.py
@@ -3,3 +3,30 @@
 # of that function to do something with it. You can have more
 # than three functions, and they don't need to call each other
 # in a circular way.
+
+def grocery_list(meat, *sides):
+    grocery_list = ", ".join(sides)
+    groceries = f"{meat}, {grocery_list}"
+    return groceries
+
+def recipe(meat, *sides):
+    """Make a meal.
+    
+    Args:
+        meat (str): Enter a type of meat e.g., chicken
+        sides (str): Enter any amount of sides e.g., corn, potatoes, pasta
+    
+    Returns:
+        dinner = the type of bread on the bottom, toppings, and another slice of bread on top.
+    """
+    side_dish = ", ".join(sides) #only works with list of strings or items that can be turned into strings
+    dish = f"{meat} with {side_dish}"
+    return dish
+
+def dinner_invite(name, dish, groceries):
+    invite = f"{name}, please join us for {dish}. I've picked up the {groceries}."
+    return invite
+
+sides = "corn, potatoes, asparagus".replace(" ", "").split(",")
+
+print(dinner_invite("Watson", recipe("chicken", *sides), grocery_list("chicken", *sides)))
