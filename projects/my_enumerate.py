0cc729d41671faf3f50dcb65dd613b17a4a8c50b
Deeter Neumann <drneumann@uwalumni.com>
2024-10-27 13:29:17 -0700

    my enumerate func

--- projects/my_enumerate.py
+++ projects/my_enumerate.py
@@ -2,6 +2,19 @@
 # Define a function called `my_enumerate()` that takes an iterable as input
 # and gives back both the element as well as its index position as an integer.
 
-def my_enumerate():  # add your arguments
-      # add your code implementation
-      pass
+def my_enumerate(list, start = 0):
+    course_index = -1
+    for course in courses:
+        course_index += 1
+        print(f"{course_index}: {course} Python")
+        
+courses = ['Intro', 'Intermediate', 'Advanced', 'Professional']
+
+my_enumerate(courses)
+
+
+# OUTPUT:
+# 0: Intro Python
+# 1: Intermediate Python
+# 2: Advanced Python
+# 3: Professional Python
