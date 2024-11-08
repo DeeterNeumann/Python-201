c7c6670dae949dfc5d67b064f112b4854aab101d
Deeter Neumann <drneumann@uwalumni.com>
2024-11-08 12:50:55 -0800

    t shirt shop

--- 06_advanced-python-concepts/06_06_shirt_shop.py
+++ 06_advanced-python-concepts/06_06_shirt_shop.py
@@ -1,5 +1,11 @@
 # Using a list comprehension, create a *cartesian product* (google this!)
 # of the given lists. Then open up your online shop ;)
 
+#from itertools import product
+
 colors = ["neon orange", "spring green"]
 sizes = ["S", "M", "L"]
+
+t_shirt_shop = [(color, size) for color in colors for size in sizes]
+
+print(t_shirt_shop)
