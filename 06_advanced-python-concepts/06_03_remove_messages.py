4350bdb9923a61311936f5256489420a73f0f625
Deeter Neumann <drneumann@uwalumni.com>
2024-11-08 11:03:35 -0800

    remove extra message

--- 06_advanced-python-concepts/06_03_remove_messages.py
+++ 06_advanced-python-concepts/06_03_remove_messages.py
@@ -4,6 +4,14 @@
 # print statement. You can't remove any code in `cook.py`,
 # but you can add code.
 
+# cook.py
+# soup = "pumpkin soup"
+
+
+# if __name__ == "__main__":
+#     print(f"{soup} is soooo good!")
+
+
 from codingnomads.cook import soup
 
 print(f"I like {soup}.")
