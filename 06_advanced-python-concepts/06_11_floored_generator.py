9d815ed82f45c0daa3242930819db51be504a5a6
Deeter Neumann <drneumann@uwalumni.com>
2024-11-09 13:25:05 -0800

    floor generator

--- 06_advanced-python-concepts/06_11_floored_generator.py
+++ 06_advanced-python-concepts/06_11_floored_generator.py
@@ -1,3 +1,10 @@
 # Adapt your Generator expression from the previous exercise:
 # Add a floor division by 1111 on it.
 # What numbers do you get?
+
+nums = range(1, 1000000)
+
+divis_gen = (num // 1111 for num in nums if num % 1111 == 0)
+
+for num in divis_gen:
+    print(num)
