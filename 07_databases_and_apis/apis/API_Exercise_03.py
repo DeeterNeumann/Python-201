f24aca2a8a8314c097b3e7f1fa7dab002f534899
Deeter Neumann <drneumann@uwalumni.com>
2024-11-19 09:41:20 -0800

    post get

--- 07_databases_and_apis/apis/API_Exercise_03.py
+++ 07_databases_and_apis/apis/API_Exercise_03.py
@@ -8,3 +8,21 @@ and create a user with your information.
 Make a GET request to confirm that your user information has been saved.
 
 '''
+
+import requests
+from pprint import pprint
+
+url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+body = {
+    "first_name": "First",
+    "last_name": "Last",
+    "email": "email@email.com"
+}
+
+response = requests.post(url, json=body)
+
+confirmation = requests.get(url)
+data = confirmation.json()
+
+pprint(data)
