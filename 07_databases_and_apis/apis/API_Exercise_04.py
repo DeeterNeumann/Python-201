e1a3aedc43866f25c878a3310d3373a4afa9efc4
Deeter Neumann <drneumann@uwalumni.com>
2024-11-19 10:25:46 -0800

    put get

--- 07_databases_and_apis/apis/API_Exercise_04.py
+++ 07_databases_and_apis/apis/API_Exercise_04.py
@@ -4,3 +4,22 @@ Write a program that makes a PUT request to update your user information to a ne
 Again make a GET request to confirm that your information has been updated.
 
 '''
+
+import requests
+from pprint import pprint
+
+url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+body = {
+    "first_name": "updated first",
+    "last_name": "updated last",
+    "email": "newemail@email.com"
+}
+
+response = requests.put(url + "/8180", json=body)
+
+print(f"Response code: {response.status_code}")
+
+response_conf = requests.get(url)
+
+pprint(f"Response content: {response_conf.content}")
